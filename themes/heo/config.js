56797d4dd462df41f57e3f47aa7ceb436ba144f2
Zin <38930476+jinzhepro@users.noreply.github.com>
2024-12-11 08:56:14 +0800

    Update config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -27,9 +27,9 @@ const CONFIG = {
   HEO_HERO_COVER_TITLE: '随便逛逛',
 
   // 英雄区显示三个置顶分类
-  HEO_HERO_CATEGORY_1: { title: '必看精选', url: '/tag/必看精选' },
-  HEO_HERO_CATEGORY_2: { title: '热门文章', url: '/tag/热门文章' },
-  HEO_HERO_CATEGORY_3: { title: '实用教程', url: '/tag/实用教程' },
+  HEO_HERO_CATEGORY_1: { title: 'JavaScript', url: '/tag/JavaScript' },
+  HEO_HERO_CATEGORY_2: { title: '数据结构与算法', url: '/tag/数据结构与算法' },
+  HEO_HERO_CATEGORY_3: { title: '随记', url: '/tag/随记' },
 
   // 英雄区右侧推荐文章标签, 例如 [推荐] , 最多六篇文章; 若留空白''，则推荐最近更新文章
   HEO_HERO_RECOMMEND_POST_TAG: '',
